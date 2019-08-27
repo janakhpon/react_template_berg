ca4d8352ebb2396cd7f66d2f5ea665f6aded02c4
janakhpon <jnovaxer@gmail.com>
2019-08-28 00:41:42 +0630

    GDEV004

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './components/header';
+import Align from './components/align';
+import Banner from './components/banner';
+import Button from './components/button';
+import Sample from './components/sample';
+import Subscription from './components/subscription';
+import Footer from './components/footer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Align/>
+        <Banner/>
+        <Button/>
+        <Sample/>
+        <Subscription/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
-export default App;
